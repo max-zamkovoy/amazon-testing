63383b1f162b1038e612de1b98a6fb1bc80299f1
Max Zamkovoy <max.zamkovoy@gmail.com>
2019-08-12 18:45:23 +0300

    Refactored Rating Filter Test

--- src/test/java/components/CustomerReviewRatingFilter.java
+++ src/test/java/components/CustomerReviewRatingFilter.java
@@ -1,12 +1,8 @@
 package components;
 
 import enums.ReviewRating;
-import org.apache.commons.collections4.CollectionUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import java.util.List;
 
 public class CustomerReviewRatingFilter {
 
@@ -17,28 +13,8 @@ public class CustomerReviewRatingFilter {
     }
 
     public void selectCustomerReviewRating(ReviewRating reviewRating) {
-        WebElement ratingLink = getRatingLink(reviewRating);
-        if (ratingLink != null) {
-            ratingLink.click();
-        }
-    }
-
-    public WebElement getRatingLink(ReviewRating reviewRating) { // TODO: SIMPLIFY BY USING CSS SELECTORS
-        WebElement ratingLink = null;
-        List<WebElement> ratings = getCustomerReviewRating();
-        if (CollectionUtils.isNotEmpty(ratings)) {
-            By ratingTag = By.cssSelector(String.format(".a-star-medium-%s", reviewRating.getRating()));
-            ratingLink = ratings
-                    .stream()
-                    .filter(element -> CollectionUtils.isNotEmpty(element.findElements(ratingTag)))
-                    .findAny()
-                    .orElse(null);
-        }
-        return ratingLink;
-    }
-
-    public List<WebElement> getCustomerReviewRating() {
-        By customerReviewRating = By.cssSelector("#reviewsRefinements .s-navigation-item");
-        return driver.findElements(customerReviewRating);
+        String reviewRatingXpath = String.format("//*[contains(@class, 'a-star-medium-%s')]/parent::a",
+                reviewRating.getRating());
+        driver.findElement(By.xpath(reviewRatingXpath)).click();
     }
 }
